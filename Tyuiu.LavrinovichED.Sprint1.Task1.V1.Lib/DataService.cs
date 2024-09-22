45b499bc833fb137f519831a71b8c73f580f9584
Tyuiu.lavrinoviched@outlook.com <Tyuiu.lavrinoviched@outlook.com>
2024-09-22 13:09:02 +0500

    Task1

--- Tyuiu.LavrinovichED.Sprint1.Task1.V1.Lib/DataService.cs
+++ Tyuiu.LavrinovichED.Sprint1.Task1.V1.Lib/DataService.cs
@@ -4,9 +4,9 @@ namespace Tyuiu.LavrinovichED.Sprint1.Task1.V1.Lib
 {
     public class DataService : ISprint1Task1V1
     {
-        public double Calculate(double a, double x, double y)
+        public double Calculate(double x, double y, double a)
         {
-            return (x / 3) / y + 6 * a; 
+            return x / 3 / y + 6 * a; 
         }
     }
 }
